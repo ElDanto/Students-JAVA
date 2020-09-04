d75cf9f3b06df8016c8beb3875143c50e7e83888
ElDanto <pavlovian.ch@gmail.com>
2020-09-04 18:22:50 +0300

    GalToLit v. 0.0.2

--- GalToLit.java
+++ GalToLit.java
@@ -2,14 +2,22 @@ import java.util.Scanner;
 
 class GalToLit {
     public static void main(String[] args) {
-        double gallons;
-        double liters;
-
+        double volume;
         Scanner in = new Scanner(System.in);
-        System.out.print("Введите обЪем :");
-        gallons = in.nextDouble();
-        liters = gallons * 3.7854;
-        System.out.printf(gallons + " галлонам соответствует: " + liters + " литра");
+        System.out.print("Из чего вы хотите переводить(l/g) :");
+        String lg = in.nextLine();
+        
+        if(lg.equals("l")){
+            System.out.print("Ваш объем в литрах :");
+            volume = in.nextDouble() / 3.7854;
+            System.out.println("Ваш обЪем в галлонах : " + volume);
+        }else if(lg.equals("g")){
+            System.out.print("Ваш объем в галлонах :");
+            volume = in.nextDouble() * 3.7854;
+            System.out.println("Ваш обЪем в литрах : " + volume);
+        }else{
+            System.out.println("Величина не распознана");
+        }
         in.close();
     }
 }
