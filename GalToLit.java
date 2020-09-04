595a12efa187b5d476a9185f2dd503beae62f7fa
ElDanto <pavlovian.ch@gmail.com>
2020-09-04 17:30:25 +0300

    Calculate Galloons to liters v. 0.0.1
    I don't know English or java, but this is my advanced 
    f*king homework

---
+++ GalToLit.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class GalToLit {
+    public static void main(String[] args) {
+        double gallons;
+        double liters;
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите обЪем :");
+        gallons = in.nextDouble();
+        liters = gallons * 3.7854;
+        System.out.printf(gallons + " галлонам соответствует: " + liters + " литра");
+        in.close();
+    }
+}
